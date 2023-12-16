fd65adc605ded3bc86799afca684bc9ee2e3c51e
Saksham <ksaksham39@gmail.com>
2023-12-16 10:07:39 +0530

    Feature: Added ts to both video routes

--- src/app/api/video/[...videoKey]/route.ts
+++ src/app/api/video/[...videoKey]/route.ts
@@ -1,31 +1,35 @@
-//add middlewares toooo
 import s3Client from "@/app/packages/s3/client";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
 import { NextRequest, NextResponse } from "next/server";
-//what this will doo
-//this will get the key of the video to be viewed
-//and will return a presigned url on the behaf of the node js connected client
-export async function GET(request: NextRequest) {
 
-const pathname = request?.nextUrl?.pathname;
-console.log(pathname);
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  // Extract the pathname from the request URL
+  const pathname: string | undefined = request?.nextUrl?.pathname;
+  console.log(pathname);
 
-const decodedPathname = decodeURIComponent(pathname);
-const videoKey = decodedPathname.split('video/').pop();
-console.log('video key',videoKey);
+  // Decode the pathname and extract the video key
+  const decodedPathname: string = decodeURIComponent(pathname || '');
+  const videoKey: string | undefined = decodedPathname.split('video/').pop();
+  console.log('video key', videoKey);
 
+  // Ensure the videoKey is defined before proceeding
+  if (!videoKey) {
+    return NextResponse.json({ error: 'Video key is missing' }, { status: 400 });
+  }
 
-
-  
-
-
+  // Create a command to get the object from S3
   const command = new GetObjectCommand({
-      Bucket: "courseifyprivatebucket",
-      Key: videoKey
-  })
-  const url = await getSignedUrl(s3Client, command);
-  return NextResponse.json({ url });
-}
+    Bucket: "courseifyprivatebucket",
+    Key: videoKey
+  });
 
+  // Generate a signed URL for the S3 object
+  try {
+    const url: string = await getSignedUrl(s3Client, command);
+    return NextResponse.json({ url });
+  } catch (error) {
+    console.error('Error generating signed URL:', error);
+    return NextResponse.json({ error: 'Error generating signed URL' }, { status: 500 });
+  }
+}
--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -1,41 +1,60 @@
+//this route returns all videos(with aws keys) for a particular course
+//protected route
 //add middlewares toooo
-import  prisma from '../../../../prisma/client'
-
-import { headers } from 'next/headers'
-
+import prisma from "../../../../prisma/client";
+import { Video, UserVideoProgress, Course } from "@prisma/client";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+interface VideoWithProgress extends Video {
+  userProgress: UserVideoProgress[];
+  course: Course;
+}
+
 export async function GET(request: NextRequest) {
-  //is route ka kaam h ek particular vcourse ki saari videos dena
+  // Extract the courseId and userId from the request headers
   const headersList = headers();
-  const courseId = headersList.get('courseId').toString();
-let userId='6573562edb4374c30a75ee00'
-  
-  
-    const courseVideos = await prisma.video.findMany({
+  const courseId: string = headersList.get("courseId")?.toString() || "";
+  let userId: string = "6573562edb4374c30a75ee00";
+
+  // Check if the courseId is provided
+  if (!courseId) {
+    return NextResponse.json({ error: "Course ID is required" }, { status: 400 });
+  }
+
+  // Check if the user is authorized (this is a placeholder for your auth logic)
+  // Replace with your actual authentication middleware
+  const userIsAuthorized: boolean = true; // Replace with actual check
+  if (!userIsAuthorized) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // Fetch the course videos and their progress
+  const courseVideos: VideoWithProgress[] | [] = await prisma.video.findMany({
     where: {
       courseId: courseId,
     },
-    include:{
-      course:true,
-      userProgress:{
-        where:{
-          userId:userId
-        }
-      }
-    }
+    include: {
+      course: true,
+      userProgress: {
+        where: {
+          userId: userId,
+        },
+      },
+    },
   });
-  const completedVideos = courseVideos.filter(video => video.userProgress[0]?.completed).length;
+
+  // Check if the course has videos
+  if (courseVideos.length === 0) {
+    return NextResponse.json({ error: "No videos found for this course" }, { status: 404 });
+  }
+
+  // Calculate the course progress
+  const completedVideos = courseVideos.filter((video) => video.userProgress[0]?.completed).length;
   const totalVideos = courseVideos.length;
   const courseProgress = totalVideos > 0 ? (completedVideos / totalVideos) * 100 : 0;
 
+  // Return the course videos and progress
   return NextResponse.json({ courseVideos, courseProgress });
-  
-  
-  
- 
-  //  now every video returned by this result has a key and we gonna then map these with our videosidemenu list items, then onlick of any of them we make a req to the video/[videokey] route which returns the signed url then we render that
 }
-
-
 
