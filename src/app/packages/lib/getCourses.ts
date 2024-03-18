7f242e082f6dd414596636a89a7d953c17c7ef5a
Saksham <ksaksham39@gmail.com>
2024-03-18 14:57:19 +0530

    updated getCourse Endpoint

--- src/app/packages/lib/getCourses.ts
+++ src/app/packages/lib/getCourses.ts
@@ -1,5 +1,5 @@
 export const getCourses=async()=>{
-    const response = await fetch("http:localhost:3000/api/courses");
+    const response = await fetch("/api/courses");
     return response.json()
 
 }
